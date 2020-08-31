7280511182533900da3f764a6547d193da480bd7
Raphael Discky <zundria.putra@gmail.com>
2020-08-31 15:39:29 +0700

    Build routes with postgreSQL queries

--- server/index.js
+++ server/index.js
@@ -5,21 +5,77 @@ const pool = require("./db");
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
 
-// Routes
-
+// ROUTES
 
 // Create todo
+app.post("/todos", async (req, res) => {
+  try {
+    // get data from client side
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // Get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("Select * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // Get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // Update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("todo was updated");
+  } catch (err) {
+    console.error();
+  }
+});
 
 // delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("todo was deleted!");
+  } catch (err){
+      console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
-    console.log("server has started on port 5000");
+  console.log("server has started on port 5000");
 });
