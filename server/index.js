761bd60f89eec92aa583458e748877c689ac33d3
Raphael Discky <zundria.putra@gmail.com>
2020-08-31 14:05:08 +0700

    Connect database and server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+
+
+// Create todo
+
+// Get all todos
+
+// Get a todo
+
+// Update a todo
+
+// delete a todo
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
